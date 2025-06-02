c34649e93b9c059715e1abb32d570b490880a5cc
gian maria ricci <ricci.gianmaria@gmail.com>
2025-06-02 14:44:52 +0200

    Root host project

--- src/Alkampfer.Assistant.Host/Program.cs
+++ src/Alkampfer.Assistant.Host/Program.cs
@@ -1,38 +1,17 @@
-using Alkampfer.Assistant.Core.LiteDbIntegration;
-using Alkampfer.Assistant.Core.Configuration;
-using Alkampfer.Assistant.Core;
-using Alkampfer.Assistant.Host;
-using Serilog;
 using Alkampfer.Assistant.Host.Components;
-using MudBlazor.Services;
 using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Configure logging with serilog
-ConfigurationHelper.ConfigureLogging();
-
-// Add Serilog to standard logging
-builder.Services.AddLogging(loggingBuilder =>
-{
-    //clear existing providers and add only serilog.
-    loggingBuilder.ClearProviders();
-    loggingBuilder.AddSerilog(dispose: true);
-});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// Add MudBlazor services
 builder.Services.AddMudServices(config =>
-{ 
-    //config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
 });
-
-// Register LiteDbRepository for IRepository<ModelDefinition>
-builder.Services.AddSingleton<IRepository<ModelDefinition>>(sp =>
-    new LiteDbRepository<ModelDefinition>("Filename=ModelDefinitions.db;Mode=Shared", "model_definitions"));
 
 var app = builder.Build();
 
